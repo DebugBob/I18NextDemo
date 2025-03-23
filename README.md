42cea1d2852def31d3a2cdab542c3862f49e2758
Robert <rastorino@outlook.com.au>
2025-03-23 12:55:32 +1100

    Fixed Arabic added Romanian
    Added Arabic Lorem as well as everything for Romanian.

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 This is a tech demo for the use of I18Next-React
 
-Clone the repo and type the following into the terminal to run in localhost: npm run dev
+Clone the repo and type the following into the terminal to run in localhost: 
+- npm install
+- npm run dev
 
 To see the effects change the language in your browsers settings, the lorem ipsum paragraph is to showcase the page flow changing from left to right, to right to left for some langauge settings (Arabic for example).
 
-The languages currently supported are English, Spanish, Arabic and Danish; So choose one of those and **refresh the page**. 
+The languages currently supported are English, Spanish, Arabic and Danish; So choose one of those from your browser language settings and **refresh the page**. 
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import es from './es.json'
+import es from "./es.json";
 
 i18n.use(initReactI18next).init({
   debug: true,
@@ -9,7 +9,8 @@ i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: {
-        homepage: { //Can give each page it's own object.
+        homepage: {
+          //Can give each page it's own object.
           form_title: "Test Form",
           name: "Name",
           email: "Email",
@@ -19,12 +20,13 @@ i18n.use(initReactI18next).init({
           correct: {
             right: "right",
           },
-          lorem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum mattis metus vitae posuere. Integer felis tortor, convallis vel sapien eget, placerat scelerisque ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed venenatis tortor nisi, at placerat nisl convallis in. Curabitur tempor non purus sollicitudin feugiat. Duis tristique sed velit ac sodales. Integer tincidunt, orci consectetur consectetur malesuada, purus nisl dapibus ipsum, ut dignissim dolor est eget ante. Integer eget massa est. Morbi a ligula est. Duis aliquet ac ante sit amet accumsan. Nam scelerisque quam risus, id molestie massa molestie in. Nam at purus consequat ex aliquam bibendum ut ac ipsum. Nunc feugiat mattis turpis, ut condimentum velit tempus eu. Sed scelerisque dui eget lorem rhoncus, commodo porttitor mi pellentesque. Nunc sit amet sodales erat, non eleifend velit. Curabitur eu libero aliquet, imperdiet leo malesuada, dignissim quam.",
+          lorem:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum mattis metus vitae posuere. Integer felis tortor, convallis vel sapien eget, placerat scelerisque ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed venenatis tortor nisi, at placerat nisl convallis in. Curabitur tempor non purus sollicitudin feugiat. Duis tristique sed velit ac sodales. Integer tincidunt, orci consectetur consectetur malesuada, purus nisl dapibus ipsum, ut dignissim dolor est eget ante. Integer eget massa est. Morbi a ligula est. Duis aliquet ac ante sit amet accumsan. Nam scelerisque quam risus, id molestie massa molestie in. Nam at purus consequat ex aliquam bibendum ut ac ipsum. Nunc feugiat mattis turpis, ut condimentum velit tempus eu. Sed scelerisque dui eget lorem rhoncus, commodo porttitor mi pellentesque. Nunc sit amet sodales erat, non eleifend velit. Curabitur eu libero aliquet, imperdiet leo malesuada, dignissim quam.",
         },
       },
     },
     es: {
-      translation: es , //Can be imported as a JSON for modularization.
+      translation: es, //Can be imported as a JSON for modularization.
     },
     da: {
       translation: {
@@ -38,7 +40,6 @@ i18n.use(initReactI18next).init({
           correct: {
             right: "korrekt",
           },
-          lorem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque condimentum mattis metus vitae posuere. Integer felis tortor, convallis vel sapien eget, placerat scelerisque ipsum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed venenatis tortor nisi, at placerat nisl convallis in. Curabitur tempor non purus sollicitudin feugiat. Duis tristique sed velit ac sodales. Integer tincidunt, orci consectetur consectetur malesuada, purus nisl dapibus ipsum, ut dignissim dolor est eget ante. Integer eget massa est. Morbi a ligula est. Duis aliquet ac ante sit amet accumsan. Nam scelerisque quam risus, id molestie massa molestie in. Nam at purus consequat ex aliquam bibendum ut ac ipsum. Nunc feugiat mattis turpis, ut condimentum velit tempus eu. Sed scelerisque dui eget lorem rhoncus, commodo porttitor mi pellentesque. Nunc sit amet sodales erat, non eleifend velit. Curabitur eu libero aliquet, imperdiet leo malesuada, dignissim quam."
         },
       },
     },
@@ -54,9 +55,28 @@ i18n.use(initReactI18next).init({
           correct: {
             right: "صحيح",
           },
+          lorem: "لكل تم ثانية السفن موالية, من لان للإتحاد الأوروبية. تمهيد المدن البشريةً هو الى, عن جعل وقبل التي ومحاولة. فمرّ تحرّك الربيع، كل بحق. حدى حقول الأرواح انتصارهم ان. بعض صفحة المدن الفرنسية ان, مدن مع سابق للحكومة, جيوب تصرّف الوراء حدى بـ. هو بعد قررت الثانية والنرويج. بـ الا تصرّف السبب اقتصادية, بعض تم معاملة الأرواح الدولارات, تعد جورج شرسة الطريق ثم. بعض من الحكم باستخدام, لعدم بالعمل هذه مع, قررت الأولية ويكيبيديا، عل هذا. اعلان الجنوبي الأبرياء بل هذه, دون بل تُصب البرية الإيطالية. مكن سكان لغات يونيو أن, وسمّيت اليابان، مع كما. بل جديدة بلديهما مدن, احداث الشرقي ٣٠ فقد. دون قد فكان الجنرال بريطانيا. فرنسا القادة بـ وصل, بـ كردة تعداد بحث."
+
         },
       },
       dir: "rtl",
+    },
+    ro: {
+      translation: {
+        homepage: {
+          form_title: "Formularul de testare",
+          name: "Nume",
+          email: "E-mail",
+          direction: {
+            right: "drept",
+          },
+          correct: {
+            right: "corect",
+          },
+          lorem:
+            "Există o mulţime de variaţii disponibile ale pasajelor Lorem Ipsum, dar majoritatea lor au suferit alterări într-o oarecare măsură prin infiltrare de elemente de umor, sau de cuvinte luate aleator, care nu sunt câtuşi de puţin credibile. Daca vreţi să folosiţi un pasaj de Lorem Ipsum, trebuie să vă asiguraţi că nu conţine nimic stânjenitor ascuns printre randuri. Toate generatoarele de Lorem Ipsum de pe Internet tind să repete bucăţi de text în funcţie de necesitate, astfel făcându-l pe acesta primul generator adevarat de pe Internet. El utilizează un dicţionar de peste 200 cuvinte din latina, combinate cu o cantitate considerabilă de modele de structuri de propoziţii, pentru a genera Lorem Ipsum care arată decent. Astfel, Lorem Ipsum-ul generat nu conţine repetiţii, infiltrări de elemente de umor sau de cuvinte non-caracteristice, etc.",
+        },
+      },
     },
   },
 });
